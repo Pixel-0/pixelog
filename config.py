9bab579eca71946757b4218f2d6f8002bc17ae53
Pixel-0 <tiny0pixel@gmail.com>
2019-08-12 11:21:50 +0300

    add quote API base url

--- config.py
+++ config.py
@@ -1,15 +1,18 @@
+import os
+
 
 class Config:
+    QUOTE_API_BASE_URL ='http://quotes.stormconsultancy.co.uk/random.json'
+    QUOTE_API_KEY = os.environ.get('QUOTE_API_KEY')
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+
+ class ProdConfig(Config):
     pass
 
-class ProdConfig(Config):
-    pass
-
-class TestConfig(Config):
-    pass
-
-class DevConfig(Config):
+ class DevConfig(Config):
     DEBUG = True
 
-config_options ={"production":ProdConfig,"default":DevConfig,"testing":TestConfig}
-
+ config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}
